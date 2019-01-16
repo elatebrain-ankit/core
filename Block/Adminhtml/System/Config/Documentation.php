5b2603e5eddf336fae9272346c5e9649aef7c250
elatebrain <45756641+elatebrain@users.noreply.github.com>
2019-01-16 13:55:42 +0530

    Release version 1.0.2

--- Block/Adminhtml/System/Config/Documentation.php
+++ Block/Adminhtml/System/Config/Documentation.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * ElateBrain
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the elatebrain.com license which is available at https://www.elatebrain.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer version in the future.
+ * If you wish to customize this extension for your needs, please refer to https://magento.com for more information.
+ *
+ * @category    Elatebrain
+ * @package     Elatebrain_Core
+ * @version     1.0.1
+ * @copyright   Copyright (c) 2019 Elatebrain (https://www.elatebrain.com/)
+ * @license     https://www.elatebrain.com/LICENSE.txt
+ */
+
 namespace Elatebrain\Core\Block\Adminhtml\System\Config;
 /**
  *
@@ -11,6 +30,6 @@ class Documentation extends \Magento\Config\Block\System\Config\Form\Fieldset
      */
     public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
-        return '<iframe id="elatebrain_store" width="100%" src="http://www.elatebrain.com/store.html?id=' . uniqid() .'" ></iframe>';
+        return '<iframe id="elatebrain_store" width="100%" src="https://www.elatebrain.com/store.html?id=' . uniqid() .'" ></iframe>';
     }
 }
--- Helper/ExtensionInfo.php
+++ Helper/ExtensionInfo.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * ElateBrain
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the elatebrain.com license which is available at https://www.elatebrain.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer version in the future.
+ * If you wish to customize this extension for your needs, please refer to https://magento.com for more information.
+ *
+ * @category    Elatebrain
+ * @package     Elatebrain_Core
+ * @version     1.0.1
+ * @copyright   Copyright (c) 2019 Elatebrain (https://www.elatebrain.com/)
+ * @license     https://www.elatebrain.com/LICENSE.txt
+ */
+
 namespace Elatebrain\Core\Helper;
 /**
  *
@@ -8,7 +27,7 @@ class ExtensionInfo extends \Magento\Framework\App\Helper\AbstractHelper
     /**
      *
      */
-    const EXTENSION_FEED_URL = "http://www.elatebrain.com/feed/extension-feed.json";
+    const EXTENSION_FEED_URL = "https://www.elatebrain.com/feed/extension-feed.json";
 
     /**
      * @return mixed
@@ -19,6 +38,8 @@ class ExtensionInfo extends \Magento\Framework\App\Helper\AbstractHelper
 
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+
         $response = curl_exec($curl);
 
         $extensionFeed = json_decode($response, true);
